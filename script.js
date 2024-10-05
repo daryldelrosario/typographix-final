8b8d7a208bb76207bd72546ba213af118e823c3a
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-10-05 15:58:34 -0400

    complete api observer js code

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // FUNCTION to create and observe Intersection Observers
+  function createObserver(selector, observerOptions, toggleClass) {
+    const items = document.querySelectorAll(selector);
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if(entry.isIntersecting) {
+          entry.target.classList.add(toggleClass);
+        } else {
+          entry.target.classList.remove(toggleClass);
+        }
+      })
+    }, observerOptions);
+
+    items.forEach((item) => {
+      observer.observe(item);
+    });
+  }
+
+  // CREATE OBSERVERS for different section
+  createObserver('#about .phrase', { root: null, threshold: 1}, 'active');
+})
+
 // NAVIGATION MENU ======================
 const nav = document.getElementById("nav");
 const menuIcon = document.querySelector(".menu-icon");
