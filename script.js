3c78ada643f47ac557b26f050f31ea2ef8fc7ef1
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-10-07 17:22:48 -0400

    modify script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // CREATE OBSERVERS for different section
-  createObserver('#about .phrase', { root: null, threshold: 1}, 'active');
+  createObserver('#about .phrase', { root: null, threshold: 1 }, 'active');
   createObserver('#gallery .image-box', { root: null, threshold: 1 }, 'active');
 })
 
