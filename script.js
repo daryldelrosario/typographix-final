6735e040d8a903d013ab4cb757649a77597b4a8f
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-10-07 17:18:01 -0400

    complete gallery partial scss file

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // CREATE OBSERVERS for different section
   createObserver('#about .phrase', { root: null, threshold: 1}, 'active');
+  createObserver('#gallery .image-box', { root: null, threshold: 1 }, 'active');
 })
 
 // NAVIGATION MENU ======================
